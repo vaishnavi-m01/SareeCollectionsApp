201a1e97ad51e1700a54b4ada45d3b62168d99fd
vaishnavi-m01 <vaishu21052004@gmail.com>
2025-03-20 10:05:43 +0530

    HomeCarousel added.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout() {
         <Stack.Screen name="welcome" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack screenOptions={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
